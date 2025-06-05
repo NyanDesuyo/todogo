6075bb36440e20a1cdbc592901004e28e49e96c4
NyanDesuyo <gerogeroe12@gmail.com>
2025-06-05 15:21:55 +0700

    model post.go

--- models/post.go
+++ models/post.go
@@ -1,8 +1,12 @@
 package models
 
+import (
+	"gorm.io/gorm"
+)
+
 // Post represents the structure of a blog post or an item.
 type Post struct {
-	ID    string `json:"id,omitempty"`
+	gorm.Model
 	Title string `json:"title"`
 	Body  string `json:"body"`
 }
